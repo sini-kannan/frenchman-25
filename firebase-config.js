2a771aaabfc26bc4c61d4b78aac7562d1a5d89f2
sini-kannan <sinitp2015@gmail.com>
2025-08-24 21:44:56 +0200

    firebase config thing

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 // Firebase config for compat SDK usage.
 // Expose as global so app.js can initialize via firebase.initializeApp(window.firebaseConfig)
 window.firebaseConfig = {
-  apiKey: "AIzaSyBDc03puBBbP5g70Dk56xbcREUGHw7wm2w",
-  authDomain: "frenchan-6d3d6.firebaseapp.com",
-  projectId: "frenchan-6d3d6",
-  storageBucket: "frenchan-6d3d6.appspot.com",
-  messagingSenderId: "772771121213",
-  appId: "1:772771121213:web:37c9c5b2650f4b2b228254",
-  measurementId: "G-8EHQT7G805"
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
 };
