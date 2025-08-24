08e303751168973d7b1698746d01c4b3a12efb73
sini-kannan <sinitp2015@gmail.com>
2025-08-24 21:54:00 +0200

    firebase config thing

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,23 @@
-// Firebase config for compat SDK usage.
-// Expose as global so app.js can initialize via firebase.initializeApp(window.firebaseConfig)
-window.firebaseConfig = {
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: import.meta.env.apiKey,
   authDomain: import.meta.env.authDomain,
   projectId: import.meta.env.projectId,
   storageBucket: import.meta.env.storageBucket,
   messagingSenderId: import.meta.env.messagingSenderId,
   appId: import.meta.env.appId,
+
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
